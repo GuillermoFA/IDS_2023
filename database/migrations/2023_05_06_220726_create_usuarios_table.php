93a714a4221b30213c4124773dc3e9bfaf3ce414
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2023-05-06 23:59:21 -0400

    Test_SubirCommit

--- database/migrations/2023_05_06_220726_create_usuarios_table.php
+++ database/migrations/2023_05_06_220726_create_usuarios_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->String('correo')->primary();
+            $table->integer('Tipo');
+            $table->String('contraseña');
+            $table->String('nombre');
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('usuarios');
     }
 };
