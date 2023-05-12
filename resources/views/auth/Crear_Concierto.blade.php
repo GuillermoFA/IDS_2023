17c699f572ae54a29f2b4fd1800ae92e203fd825
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2023-05-12 02:16:53 -0400

    Requerimiento CL-002

--- resources/views/auth/Crear_Concierto.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime velit voluptas provident rem voluptate repellendus ea harum nobis iure amet? Incidunt unde, eos impedit aut voluptate qui. Aperiam, reiciendis odio.</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\registerUser;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/login', function () {
+//  return view('login');
+//})->name('login');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'loginAuth'])->name('loginAuth');
+Route::get('/', [LoginController::class, 'welcome'])->name('welcome');
+
