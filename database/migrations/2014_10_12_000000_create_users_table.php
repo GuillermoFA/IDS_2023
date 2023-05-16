0854861cac7d7729cab2477abfa0120aba0e7e5c
Ertinho <105566974+Ertinho@users.noreply.github.com>
2023-05-16 18:39:56 -0400

    Ordenamiento de archivos y codigo
    Cambio de controladores, nombre modelo de concierto, borrar paginas blade, añadir mensajes en myhelper, cambio en tabla de migracion de usuario(email unique).

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
 
             $table->id();
-            $table->string('email')->unique;
+            $table->string('email')->unique();
             $table->string('password');
             $table->integer('role');
             $table->string('name');
--- resources/views/auth/Crear_Concierto.blade.php
+++
@@ -1,8 +0,0 @@
-
-<h1>aljdhaskljda</h1>
-
-<button link="/ProyectoMelody/Resources/views/auth/Inicio.blade.php">Boton
-
-    <a href="href=/ProyectoMelody/Resources/views/auth/Inicio.blade.php"></a>
-
-</button>
