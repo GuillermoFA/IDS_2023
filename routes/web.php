88676f66f69589c285c0e630a00bc8b5f9a314d1
Ertinho <105566974+Ertinho@users.noreply.github.com>
2023-06-18 01:59:09 -0400

    ComprarEntradas
    -Vista de la venta de entradas.
    -Vista de un constructor de pdfs.
    -Modificación del modelo de Sales y migración.
    -Añadir un color más a los estilos css.
    -Instalar DOMPDF.
    -Añadir sección de script en app.blade por encima de alertas.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\ConcertController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,7 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('layouts.dashboard');
+    return view('login');
 });
 
 Route::get('/register', [RegisterController::class, 'index']);
@@ -38,5 +39,10 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('concert', [ConcertController::class, 'store'])->name('concert');
     Route::get('concert', [ConcertController::class, 'create'])->name('concert.create');
 });
+
+
+Route::get('/concert-order/{id}', [SalesController::class, 'create'])->name('concert.buy');
+Route::post('/concert-order/{id}', [SalesController::class, 'store'])->name('concert.order.pay');
+Route::get('download-pdf/{id}', [SalesController::class, 'downloadPDF'])->name('pdf.download');
 
 
